17084d84f5a32d19d233e8bcad25b24ab017e1c8
Sybatron <vipsister2014@gmail.com>
2024-01-27 17:12:10 +0200

    Updated config files

--- src/webapp/.eslintrc.cjs
+++ src/webapp/.eslintrc.cjs
@@ -1,18 +1,57 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
+
+/** @type {import("eslint").Linter.Config} */
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+	overrides: [
+		{
+			extends: [
+				"plugin:@typescript-eslint/recommended-requiring-type-checking",
+			],
+			files: ["*.ts", "*.tsx"],
+			parserOptions: {
+				project: path.join(__dirname, "tsconfig.json"),
+			},
+		},
+	],
+	root: true,
+	env: { browser: true, es2020: true },
+
+	extends: [
+		"plugin:@typescript-eslint/recommended",
+		"plugin:react-hooks/recommended",
+	],
+	ignorePatterns: ["dist", ".eslintrc.cjs"],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		project: path.join(__dirname, "tsconfig.json"),
+	},
+	plugins: ["@typescript-eslint,react-refresh"],
+	rules: {
+		"@typescript-eslint/consistent-type-imports": [
+			"warn",
+			{
+				prefer: "type-imports",
+				fixStyle: "inline-type-imports",
+			},
+		],
+		"@typescript-eslint/no-unused-vars": [
+			"warn",
+			{ argsIgnorePattern: "^_" },
+		],
+		"@typescript-eslint/no-misused-promises": [
+			"error",
+			{
+				checksVoidReturn: {
+					arguments: false,
+					attributes: false,
+				},
+			},
+		],
+		"no-console": "warn",
+		"react-refresh/only-export-components": [
+			"warn",
+			{ allowConstantExport: true },
+		],
+	},
+};
