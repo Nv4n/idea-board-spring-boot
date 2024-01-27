3678dbdd1fb768d52b1abc0bfbee177c18484021
Sybatron <vipsister2014@gmail.com>
2024-01-27 16:00:06 +0200

    Project Spring Boot init

---
+++ src/main/java/com/spring/boot/ideaboard/IdeaBoardSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.ideaboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaBoardSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdeaBoardSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/boot/ideaboard/IdeaBoardSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.boot.ideaboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaBoardSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
