748eb9b5150e6eca0be086526b1721481093302b
Sybatron <vipsister2014@gmail.com>
2024-02-11 03:02:03 +0200

    Java update

--- src/main/java/com/ideaboard/service/ChatService.java
+++ src/main/java/com/ideaboard/service/ChatService.java
@@ -1,8 +1,6 @@
 package com.ideaboard.service;
 
-import com.ideaboard.model.dto.ChatDto;
-
 public interface ChatService {
-    void createChat(ChatDto chatDto);
-    
+    void createChat();
+
 }
--- src/main/java/com/ideaboard/service/impl/ChatServiceImpl.java
+++ src/main/java/com/ideaboard/service/impl/ChatServiceImpl.java
@@ -1,13 +1,12 @@
 package com.ideaboard.service.impl;
 
-import com.ideaboard.model.dto.ChatDto;
 import com.ideaboard.service.ChatService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ChatServiceImpl implements ChatService {
     @Override
-    public void createChat(ChatDto chatDto) {
+    public void createChat() {
 
     }
 }
